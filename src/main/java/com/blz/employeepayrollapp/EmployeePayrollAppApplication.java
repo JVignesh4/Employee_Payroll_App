47ddadfca635281cd81d1e91afc7960659aa70e9
JVignesh4 <vigneshbuji04@gmail.com>
2022-09-24 12:46:41 +0530

    ADDED : Used Lombok For Logging Levels And Files And Patterns Are Han… …dled In application.properties

--- src/main/java/com/blz/employeepayrollapp/EmployeePayrollAppApplication.java
+++ src/main/java/com/blz/employeepayrollapp/EmployeePayrollAppApplication.java
@@ -1,13 +1,16 @@
 package com.blz.employeepayrollapp;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class EmployeePayrollAppApplication {
 
     public static void main(String[] args) {
         System.out.println("Welcome To Employee Payroll Application!");
         SpringApplication.run(EmployeePayrollAppApplication.class, args);
+        log.info("Employee Payroll App Started");
     }
 }
--- src/main/java/com/blz/employeepayrollapp/controller/EmployeePayrollController.java
+++ src/main/java/com/blz/employeepayrollapp/controller/EmployeePayrollController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.*;
 
 
 import java.util.List;
-
 @RestController
 @RequestMapping("/employeepayrollservice")
 public class EmployeePayrollController {
