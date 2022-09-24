4ba13db9af5bc738f6f3ba344775a8c549f3607c
JVignesh4 <vigneshbuji04@gmail.com>
2022-09-24 13:18:05 +0530

    Added: Added Remaining Properties To The Payroll DTO And Model

--- src/main/java/com/blz/employeepayrollapp/model/EmployeePayrollData.java
+++ src/main/java/com/blz/employeepayrollapp/model/EmployeePayrollData.java
@@ -1,6 +1,10 @@
 package com.blz.employeepayrollapp.model;
+
 import com.blz.employeepayrollapp.dto.EmployeePayrollDTO;
 import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Data
 public class EmployeePayrollData {
@@ -8,13 +12,29 @@ public class EmployeePayrollData {
     private int employeeId;
     private String name;
     private long salary;
+    private String gender;
+    private LocalDate startDate;
+    private String note;
+    private String profilePic;
+    private List<String> departments;
 
     public EmployeePayrollData() {
+
     }
 
     public EmployeePayrollData(int employeeId, EmployeePayrollDTO employeePayrollDTO) {
         this.employeeId = employeeId;
+        this.updateEmployeePayrollData(employeePayrollDTO);
+    }
+
+    public void updateEmployeePayrollData(EmployeePayrollDTO employeePayrollDTO) {
         this.name = employeePayrollDTO.name;
         this.salary = employeePayrollDTO.salary;
+        this.gender = employeePayrollDTO.gender;
+        this.note = employeePayrollDTO.note;
+        this.startDate = employeePayrollDTO.startDate;
+        this.profilePic = employeePayrollDTO.profilePic;
+        this.departments = employeePayrollDTO.departments;
+
     }
 }
